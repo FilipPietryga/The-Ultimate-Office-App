e32ef36cdf3c760d507216a268574199a2058a48
FilipPietryga <filippatrykpietryga@gmail.com>
2025-01-09 23:39:15 +0100

    initialized backend

---
+++ backend/src/test/java/ultimate_office_web_app/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ultimate_office_web_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
